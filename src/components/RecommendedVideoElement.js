a73146518c6cf47f96840eb8d13c4bb2a27e64b1
itznasrullah <nasirnagpur@gmail.com>
2023-08-28 16:00:23 +0530

    Fixing title length

--- src/components/RecommendedVideoElement.js
+++ src/components/RecommendedVideoElement.js
@@ -3,9 +3,9 @@ import './RecommendedVideoElement.css'
 const RecommendedVideoElement = (props) => {
     return (
         <div className='RecommendedVideoElement'>
-            <img className='search-video' src={props.src} alt={props.title} />
+            <img className='search-video' src={props.src} alt={props.title.slice(0, 60)} />
             <div className='title-views-ago'>
-                <p className='Video-title'>{props.title}</p>
+                <p className='Video-title'>{props.title.slice(0, 60)}</p>
                 <p className='Video-channel'>{props.channel}</p>
                 <div className='Views-TimeAgo'>
                     <p>{props.views}</p>
